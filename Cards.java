99a32a45a84fdf9b3b8305b4630771bbaf1d29c3
nknair1 <145375853+nknair1@users.noreply.github.com>
2024-05-08 11:40:53 -0700

    CardsUp1.java
    Updated Card Class

--- Cards.java
+++ Cards.java
@@ -1,38 +1,34 @@
-public class Cards {
-    private String suit;
-    private String rank;
-
-    public Cards(String suit, String rank) {
+public class Card {
+    private String suit; // "Hearts", "Diamonds", "Clubs", "Spades"
+    private String rank; // "Ace", "2", "3", ..., "10", "Jack", "Queen", "King"
+    // Class constructor
+    public Card(String suit, String rank) {
         this.suit = suit;
         this.rank = rank;
     }
-
+    // Gets suit of Card
     public String getSuit() {
         return suit;
     }
-
+    // Gets rank of Card
     public String getRank() {
         return rank;
     }
 
+    // Method to get the value of the card for Blackjack
     public int getValue() {
-        int value;
-        try {
-            value = Integer.parseInt(rank); 
-        } catch (NumberFormatException e) {
-            switch (rank) {
-                case "Ace":   value = 11; break;
-                case "Jack":  
-                case "Queen": 
-                case "King":  value = 10; break;
-                default:       value = 0; break; 
-            }
+        if (rank.equals("Ace")) {
+            return 11; // Ace can be 1 or 11, initially set to 11
+        } else if (rank.equals("King") || rank.equals("Queen") || rank.equals("Jack")) {
+            return 10;
+        } else {
+            return Integer.parseInt(rank); // Numerical ranks (2-10)
         }
-        return value;
     }
-    
+
+    // Override toString() method for easy printing
     @Override
     public String toString() {
         return rank + " of " + suit;
     }
-}
+} 
