46819477ea90fb717c2eeea885a8ceb5ef9c9579
nknair1 <145375853+nknair1@users.noreply.github.com>
2024-05-08 11:41:36 -0700

    DeckUp1.java
    Updated Deck Class

--- Deck.java
+++ Deck.java
@@ -2,36 +2,39 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck {
-    private ArrayList<Cards> cards;
-
+    private ArrayList<Card> cards;
+    // Constructor
     public Deck() {
-        cards = new ArrayList<>();
-        createDeck();
+        cards = new ArrayList<Card>();
     }
-
-    private void createDeck() {
+    // Creates a Deck
+    public void createDeck() {
+        // Suits and ranks for a standard deck of cards
         String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
         String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
+        // Create all 52 cards
         for (String suit : suits) {
             for (String rank : ranks) {
-                cards.add(new Cards(suit, rank));
+                cards.add(new Card(suit, rank));
             }
         }
     }
-
+    // Gets all Cards in Deck
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+    // Shuffles Deck
     public void shuffle() {
-        Collections.shuffle(cards);
+        Collections.shuffle(cards);  // Use the built-in shuffle method
     }
-
-    public Cards dealCard() {
-        if (cards.isEmpty()) {
-            return null; 
+    // Deals Cards. Will pass to addCard method in Hand Class to be Used In Game Class
+    public Card dealCard() {
+        if (cards.size() > 0) {
+            return cards.remove(0); // Remove and return the top card
+        } else {
+            return null; // Return null if the deck is empty
         }
-        return cards.remove(0); 
     }
-    
-    public boolean isEmpty() {
-        return cards.isEmpty();
-    }
+   
 }
